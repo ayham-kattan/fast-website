cb5b3f78e898c8e6239f48a121169a3e53e537e5
aak201610885 <aak201610885.com>
2021-08-18 01:14:09 +0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+// import movies from './movies'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// const [state, setstate] = useState(movies)
+	return (
+		<div className="App">
+			<header><h1>movie</h1></header>
+			<main></main>
+			<footer> this my footer</footer>
+		</div>
+	);
 }
 
 export default App;
